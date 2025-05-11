62e4ac39aee315d1300139c62b40cdfc39a0973b
vivekbiradar023 <vivekbiradar11@gmail.com>
2025-05-11 23:54:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".fade-section");
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => observer.observe(section));
+});
